085adb78eb80bfa227e325b19cb6c0afea60be56
kevinKrazius <kevin.krazius@techstarter.de>
2024-02-07 15:50:28 +0100

    Kevin Aktualisierung

--- src/index.js
+++ src/index.js
@@ -7,18 +7,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./layout/navbar/Navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    {/* <Navbar />
-    <App /> */}
-    *{" "}
+  
     <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
-    *
+    
   </React.StrictMode>
 );
 
